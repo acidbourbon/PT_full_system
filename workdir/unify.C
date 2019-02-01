ab1bec0f371d4cf4b011fe51d0c1330eef9b2bf9
Hades DAQ <hadaq@linux-l8n8.site>
2019-02-01 17:10:00 +0100

    meta display for TDCs in unify.C

--- workdir/unify.C
+++ workdir/unify.C
@@ -165,12 +165,17 @@ void unify(void){
   
   out_file->cd();
   joint_tree->Write();
-  out_file->Close();
-  delete out_file;
+//   out_file->Close();
+//   delete out_file;
   
   ///// examples for drawing ////
   
-  // joint_tree->Draw("hits.t1","hits.chan == 350005")
+//   joint_tree->Draw("hits.t1","hits.chan == 350005");
+//   joint_tree->Draw("hits.t1","hits.chan > 350*1000 && hits.chan < 350*1001");
+  new TCanvas();
+  joint_tree->Draw("hits.chan : hits.t1>>t1_meta(1000,200,250,16,350001,350016)","hits.chan > 350*1000 && hits.chan < 350*1001","colz");
+  new TCanvas();
+  joint_tree->Draw("hits.chan : hits.tot>>tot_meta(1000,0,400,16,350001,350016)","hits.chan > 350*1000 && hits.chan < 350*1001","colz");
 
   
   
