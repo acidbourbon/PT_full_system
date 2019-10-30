b37b212913db80bf694ba5e4a387cdda2d93b05c
Michael Wiebusch <m.wiebusch@gsi.de>
2019-10-30 14:27:15 +0100

    phidrive working

--- build_and_run_root.sh
+++ build_and_run_root.sh
@@ -3,8 +3,9 @@ docker build -t go4 . || exit
 docker run --net host -v $(pwd)/conf:/conf -v $(pwd)/workdir:/workdir --rm -it \
 --device /dev/serial/by-id/usb-FTDI_USB__-__Serial-if00-port0:/dev/ttyUSB_micos_eco \
 --device /dev/serial/by-id/usb-HAMEG_HAMEG_HO720_023192710-if00-port0:/dev/ttyUSB_HAMEG_TRB \
---device /dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0:/dev/ttyUSB_phidrive \
+--device /dev/serial/by-id/usb-HAMEG_HAMEG_HO720_020546035-if00-port0:/dev/ttyUSB_HAMEG_LV \
 --device /dev/serial/by-id/usb-Arduino_LLC_Arduino_Leonardo-if00:/dev/ttyUSB_wallplugs \
+--device /dev/serial/by-id/usb-Arduino_LLC_Arduino_Leonardo_phidrive-if00:/dev/ttyUSB_phidrive \
 --name go4 \
 go4 /workdir/start.sh
 
---
+++ workdir/python_modules/phidrive.py
@@ -0,0 +1,36 @@
+import re
+
+import time
+
+import serial
+
+device = '/dev/ttyUSB_phidrive'
+baudrate = 115200
+
+
+def send_cmd(cmd,**kwargs):
+  timeout = float(kwargs.get("timeout",1))
+
+  with serial.Serial(device, baudrate, timeout=timeout) as ser:
+    ser.write(str.encode(cmd)+b"\r\n")
+    line = ser.readline().decode()
+    ser.close()
+    line.replace("\r","")
+    line.replace("\n","")
+    return line 
+
+def read_line(**kwargs):
+  timeout = float(kwargs.get("timeout",1))
+  with serial.Serial(device, baudrate, timeout=timeout) as ser:
+    line = ser.readline().decode()
+    ser.close()
+    line.replace("\r","")
+    line.replace("\n","")
+    return line 
+
+def set_angle(deg):
+  send_cmd(str(deg))
+  time.sleep(4)
+
+def read_angle():
+  return float(read_line().replace("pos degree ",""))
