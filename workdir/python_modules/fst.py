b39815506051eb763eaa10f1456c4a00eca63d98
Michael Wiebusch <m.wiebusch@gsi.de>
2019-11-06 18:55:47 +0100

    snap before baseline calib fix attempt

--- workdir/python_modules/fst.py
+++ workdir/python_modules/fst.py
@@ -7,7 +7,7 @@ def take_data(**kwargs):
       os.system("cd /workdir; ./take_data_n_sec.sh {:f}".format(time))
     else:
       os.system("cd /workdir; ./take_data_n_evt.sh {:d}".format(events))
-    os.system("cd /workdir; root -b -l unify.C -q")
+    #os.system("cd /workdir; root -b -l unify.C -q")
 
     
 
@@ -18,3 +18,29 @@ def trigger_scinti():
 def trigger_ufsd():
   import os
   os.system("cd /conf; ./trigger_ufsd.sh")
+    
+def wait_for_spill():
+ # untested
+ import scaler
+ trigger_rate_threshold = 4000
+ poll_acq_time = 1
+ ### wait for spill start
+ trig_chan = 30
+ while True:
+   curr_rates = scalers.scaler_rate("0x0351",[trig_chan],poll_acq_time)
+   scint_rate = curr_rates[0]
+   if scint_rate > trigger_rate_threshold*poll_acq_time:
+     print("## spill start ##")
+     break
+   else:
+     print(".", end=" ")
+        
+#    ### wait for spill break
+#    while True:
+#      curr_rates = scalers.scaler_rate("0x0351",[dut_chan,trig_chan],poll_acq_time)
+#      scint_rate = curr_rates[1]
+#      if scint_rate < trigger_rate_threshold*poll_acq_time:
+#        print("## spill break ##")
+#        break
+#      else:
+#        print(":", end=" ")
