62262af5b21bd7e64b90d437cc86d9193452bf5e
Michael Wiebusch <m.wiebusch@gsi.de>
2019-11-07 21:33:43 +0100

    some ideas by t410

--- workdir/correlation.sh
+++ workdir/correlation.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-root -l correlation.C
+root -l correlation.C -b -q
--- workdir/quick_fish_offline.sh
+++ workdir/quick_fish_offline.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-data_dir=$
-./go4_offline.sh $data_dir
-./correlations.sh 
-cp joint_tree.root correlations.root $data_dir/
+number=1000
+data_dir=$1
+if [ -e $data_dir/joint_tree_prev$number.root ]; then
+echo "was already unpacked"
+#root -l $data_dir/*.root
+else
+./go4_offline.sh $data_dir/*.hld -number $number
+./correlation.sh 
+cp joint_tree.root  $data_dir/joint_tree_prev$number.root
+cp correlation.root $data_dir/correlation_prev$number.root
+echo "unpacking"
+fi
