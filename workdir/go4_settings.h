bbb57a59836085847a5c518448d111475bfef21a
hadaq@l8n8 <hadaq@l8n8>
2019-12-03 15:57:30 +0100

    snapshot with sergey

--- workdir/go4_settings.h
+++ workdir/go4_settings.h
@@ -3,7 +3,7 @@
 
 #define CHANNELS 53
 #define max_baseline_stddev 2.5
-#define reference_channel 35103
+#define reference_channel 35049
 #define spike_rejection 15
 #define spike_rejection_refchan 10
 #define t1_L -400
@@ -11,13 +11,12 @@
 #define threshold_noise_scan_limit 64
 #define tot_L -10
 #define tot_R 2000
-#define uncommented_reference_channel 35049
-#define HUBRANGE_START 0xc035
+#define HUBRANGE_START 0x8150
 #define HUBRANGE_STOP 0xc035
-#define TDCRANGE_START 0x0351
-#define TDCRANGE_STOP 0x0351
+#define TDCRANGE_START 0x0350
+#define TDCRANGE_STOP 0x1503
 
 
 // second processes in second.C:
-#define SECOND_PROCESS_TDCs new SecondProc("Sec_0351", "TDC_0351");
+#define SECOND_PROCESS_TDCs new SecondProc("Sec_0350", "TDC_0350");new SecondProc("Sec_0351", "TDC_0351");new SecondProc("Sec_0352", "TDC_0352");new SecondProc("Sec_0353", "TDC_0353");new SecondProc("Sec_1500", "TDC_1500");new SecondProc("Sec_1501", "TDC_1501");new SecondProc("Sec_1502", "TDC_1502");new SecondProc("Sec_1503", "TDC_1503");
 
